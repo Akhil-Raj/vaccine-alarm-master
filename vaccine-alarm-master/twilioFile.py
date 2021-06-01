4822a136e8882ab7ca45534664d1d7aea3ec781a
Akhil Raj <akhil.raj1997@gmail.com>
2021-06-01 20:41:42 +0530

    Update twilioFile.py

--- vaccine-alarm-master/twilioFile.py
+++ vaccine-alarm-master/twilioFile.py
@@ -2,11 +2,11 @@
 from twilio.rest import Client
 
 # Your Account SID from twilio.com/console
-account_sid = "AC4f20b5df995843f5b73d443a4bc321db"
+account_sid = ""
 # Your Auth Token from twilio.com/console
-auth_token  = "6c6f842638b2fbaa94769c48c294ab9b"
-to_number = "+919045907963"
-from_number = "+13392934573"
+auth_token  = ""
+to_number = ""
+from_number = ""
 body_msg = "Hello! You got a message from the vaccine-checker!"
 
 client = Client(account_sid, auth_token)
